ccc413d580ba90abf9cefda685d8a2d8facfd958
movavi_school <zoyaterina@mail.ru>
2023-01-24 21:26:59 +0700

    аа

---
+++ 18 lesson/script.js
@@ -0,0 +1,16 @@
+/*let h = 170;
+let m = 55;*/
+//imt = prompt("ваш ИМТ");
+/*let i1 =  m / (h/100) ** 2;
+
+let hc = 100;
+let mc = 3;
+let i2 =  mc / (hc/100) ** 2;
+alert(i1>i2)*/
+//console.log(`hello + (message)`);
+
+/*const name = "ivan";
+const job = "teacher";
+const firstJobYear = 2014;
+const today = 2023;
+console.log("прошу внимание, это ваш новый учитель," + name + job + firstJobYear + today)*/
