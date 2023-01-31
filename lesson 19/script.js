aafca6d4eb1947b82307c00e8ced7dcfe7cea8c0
movavi_school <mdiscool1@mail.ru>
2023-01-31 21:33:51 +0700

    31 января

---
+++ lesson 19/script.js
@@ -0,0 +1,25 @@
+/*years = prompt("ваш возраст");
+
+age = (18 - years)
+if (years >= 18) {
+    alert("можете проходить")
+} else {
+    alert(`подожди ещё ${age} лет чел`)
+}*/
+
+x = 'nsk'
+switch (x) {
+    case 'nsk':
+        console.log("привет"); 
+        break;
+    case 'spb': 
+        console.log("пока"); 
+        break;
+    default:
+       reak;
+} console.log("чел ты"); 
+        b
+
+/*alert()*/
+
+newDate.getDate()
