a62ae0aa9c97870907cfa04597972641801ccbd0
movavi_school <Top1invokerdotabuff@mail.ru>
2023-02-14 21:25:38 +0700

    888

---
+++ 21 lesson/script.js
@@ -0,0 +1,86 @@
+/*l = [1, 5, 6];
+console.log(l);
+console.log(l.lenght);*/
+
+/*function year(gh){
+    let x = now - gh[0];
+    let y = now - gh[1];
+    let z = now - gh.lenght - 1;
+}
+now = 2023;
+gh = [1920, 2013, 2020, 1996, 1630]
+year(gh)
+cconsole.log(x, y, z)*/
+
+/*let l=[1 ,3 ,"f" ,true ,"ширина"];
+let len = l.push("ogyrec");
+console.log(l);
+
+let deleted = l.pop()
+l.p
+console.log(l)*/
+
+//l.unshift("uno");                                  добавляет элемент в начало 
+
+
+
+
+
+//l.shift();                                        удаление первого элемента
+
+
+
+
+
+//console.log(l.indexOf(true));                      поиск индекса в массиве
+//console.log(l.indexOf("oooo"));                    ненайденый индекс даст -1
+
+//console.log(l.includes("f"));                         правда/ложь элемента
+
+/*
+function nilckname(){
+    console.log(name.includes(nickname))
+    if (name.includes(nickname) == true) {
+        alert("уже есть")
+    }   else {
+        alert("котяра")
+    }
+}
+let name = ["овощ", "огурец", "картошка", "кошка", "пес", "кити", ]
+let nickname = prompt("никнейм?")
+nilckname()*/
+
+
+/*const object = {
+    key1 : "value1",
+    ke2 : "value2",                                                  обьекты 
+    "key3 example":"value3"
+}
+console.log(object)*/
+
+/*
+console.log(teacher.firstName);                                         1 способ
+                                                               получение данных из обьектов
+console.log(teacher["firstName"];                                       2 способ
+*/
+
+//console.log(teacher["first" + commonPart])                      сложение ключей
+
+/*teacher.location = "нск"
+teacher["location"] = "нск"                                         добавить запись в обьект
+console.log(teacher)*/
+
+
+
+
+                                                //обьект может содержать функцию 
+
+
+
+
+/*console.log(teacher.calcAge(1999));              так 
+                                                   или            пользование функцией которая внутри обьекта
+console.log(teacher.["calcAge"](1999));            так
+*/
+
+//68 слайд
