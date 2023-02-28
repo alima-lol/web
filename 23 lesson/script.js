f4ceb8aa864fc9961a46c5b019eb1b9bfa0891c9
movavi_school <zoyaterina@mail.ru>
2023-02-28 21:26:01 +0700

    hhhhhhhh

---
+++ 23 lesson/script.js
@@ -0,0 +1,48 @@
+/*for(let jump=1; jump<=10; jump++){
+   if (jump == 4) continue;                                                    обход цикла 
+   console.log("прыг" + jump)
+}
+
+
+for(let jump=1; jump<=10; jump++){
+    if (jump == 4) break;                                                       остановка цикла
+    console.log("прыг" + jump)
+ }*/
+
+ /*for(let jump=1; jump<=10; jump++){
+    if (jump == 4) continue;
+    console.log("прыг" + jump)
+    for(let prised =1; prised <=3; prised++){ 
+        if (  jupm % 2 == 0) break; 
+        console.log("приседание" + prised)
+    }                                                  
+ }*/
+
+
+/*let jump = 1;
+while (jump <= 10){
+    console.log(jump)
+    jump++;
+} */
+
+
+
+/*x = Math.trunc(Math.random()*6) + 1;
+console.log(x)*/
+/*console.log(Math.trunc(x))                                         //округление вниз
+console.log(Math.ceil(x))                                          //округление вверх 
+console.log(Math.round(x))                                         //матем округление
+console.log(x.toFixed(2))                                             
+
+
+
+console.log(1.35.toFixed(20))            
+console.log(6.35.toFixed(20))*/                                       
+ 
+
+
+
+document.querySelector(".message").innerText = "hihi"
+
+
+document.querySelector(".class").value = "ok"
